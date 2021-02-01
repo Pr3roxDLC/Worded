63f1f554b578da17e38d9613ab1e411a065c7faa
Pr3roxDLC <57639545+Pr3roxDLC@users.noreply.github.com>
2021-02-01 18:38:52 +0100

    Reworked Checks for Better UI later

--- src/Main.java
+++ src/Main.java
@@ -18,11 +18,61 @@ public class Main extends JFrame implements Runnable {
 
     JPanel panel = new JPanel();
 
+    public enum ProgramState {STARTED, INITIALIZING_RUN, INITIALIZED_RUN, RUNNING, FINISHED}
+
+    ProgramState programState = ProgramState.STARTED;
+
+    File wordList = null;
+    Scanner scanner = null;
 
     @Override
     public void run() {
 
         while (true) {
+
+            if(programState == ProgramState.INITIALIZING_RUN){
+                wordList = new File("C:\\Users\\Tim\\Documents\\wordlist.txt");
+                try {
+                    scanner = new Scanner(wordList);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                programState = ProgramState.INITIALIZED_RUN;
+            }
+            if(programState == ProgramState.INITIALIZED_RUN){
+
+                if(scanner.hasNextLine()){
+
+                    try {
+                        String data = scanner.nextLine();
+
+                        if (data.length() <= 3) continue;
+                        testLabel.setText(data);
+                        URL oracle = new URL("https://api.mojang.com/users/profiles/minecraft/" + data);
+                        URLConnection yc = oracle.openConnection();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+                        String inputLine;
+                        StringBuilder str = new StringBuilder();
+                        while ((inputLine = in.readLine()) != null) {
+                            str.append(inputLine);
+                        }
+                        if (str.toString().equalsIgnoreCase("")) {
+                            System.out.println("AVAILABLE: " + data);
+                        } else {
+                            System.out.print(".");
+                        }
+                        in.close();
+                        //MojangAPI Rate Limit 600 / 10Mins, Bypass with Proxies?
+                        Thread.sleep(1000);
+                    }catch (IOException | InterruptedException e){
+
+                        e.printStackTrace();
+
+                    }
+                }
+
+            }
+
 
             try {
                 Thread.sleep(1);
@@ -45,11 +95,8 @@ public class Main extends JFrame implements Runnable {
         goButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    go();
-                } catch (IOException | InterruptedException ex) {
-                    ex.printStackTrace();
-                }
+
+                programState = ProgramState.INITIALIZING_RUN;
 
             }
         });
@@ -60,6 +107,7 @@ public class Main extends JFrame implements Runnable {
 
         testLabel.setLocation(50, 50);
         testLabel.setBounds(50, 50, 50, 12);
+        testLabel.setForeground(Color.WHITE);
         testLabel.setText("TestTestTest");
         testLabel.setVisible(true);
         panel.add(testLabel);
@@ -79,6 +127,8 @@ public class Main extends JFrame implements Runnable {
         while (scanner.hasNextLine()) {
             String data = scanner.nextLine();
 
+            if (data.length() <= 3) continue;
+            testLabel.setText(data);
             URL oracle = new URL("https://api.mojang.com/users/profiles/minecraft/" + data);
             URLConnection yc = oracle.openConnection();
             BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
@@ -87,10 +137,10 @@ public class Main extends JFrame implements Runnable {
             while ((inputLine = in.readLine()) != null) {
                 str.append(inputLine);
             }
-            if (str.toString().equalsIgnoreCase("")){
+            if (str.toString().equalsIgnoreCase("")) {
                 System.out.println("AVAILABLE: " + data);
-            }else{
-               System.out.print(".");
+            } else {
+                System.out.print(".");
             }
             in.close();
             //MojangAPI Rate Limit 600 / 10Mins, Bypass with Proxies?
